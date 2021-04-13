5097c22181471194e583c095125b82448fc1cb29
bchangg32 <79727151+bchangg32@users.noreply.github.com>
2021-04-13 13:59:46 -0500

    Update AirlineReaderInterface.java

--- ProjectThreeFinalApp/ShareFolder/AirlineReaderInterface.java
+++ ProjectThreeFinalApp/ShareFolder/AirlineReaderInterface.java
@@ -3,8 +3,19 @@ import java.io.Reader;
 import java.util.ArrayList;
 import java.util.zip.DataFormatException;
 
-public interface AirlineReaderInterface{
-    public ArrayList<String> getAllAirports(Reader inputFileReader) throws IOException, DataFormatException;
-    
-    public ArrayList<Airline> getAllAirlines(Reader inputFileReader) throws IOException, DataFormatException;
+// --== CS400 File Header Information ==--
+// Name: Brendan Chang
+// Email: bchang32@wisc.edu
+// Team: Red
+// Role: Front End Developer
+// TA: Xinyi Ju
+// Lecturer: Florian Heimerl
+// Notes to Grader:
+
+public interface AirlineReaderInterface {
+  public ArrayList<String> getAllAirports(Reader inputFileReader)
+      throws IOException, DataFormatException;
+
+  public ArrayList<Airline> getAllAirlines(Reader inputFileReader)
+      throws IOException, DataFormatException;
 }
